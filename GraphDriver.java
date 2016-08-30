33900044862c3ead337b76800a6f7762011279ee
Dhiren Navani <navanidhiren@gmail.com>
2016-08-30 11:49:55 -0700

    Added DFS

--- GraphDriver.java
+++ GraphDriver.java
@@ -23,12 +23,16 @@ Graph(int size){
 		graph[i]=new Node();
 		}
 	}
+
+public int size(){
+return graph.length;
+}
 public void addEdge(int source, int destination, int edgeweight){
-	System.out.println(graph[source]);
+	//System.out.println(graph[source]);
 	Edge e=new Edge();
 	e.destination=destination;
 	e.weight=edgeweight;
-	System.out.println(graph);
+	//System.out.println(graph);
 	graph[source].neighbours.add(e);
 	
 	}
@@ -65,19 +69,51 @@ public void display(){
 			}
 		}
 	}
+public List<Edge> getNeighbours(int ip){
+	return(graph[ip].neighbours);
+	}
 }
+
+
+
+class GraphAlgorithms{
+public static void dfs(int source, Graph input, boolean visited[]){
+visited[source]=true;
+System.out.println(source);
+List<Edge> neighbours=input.getNeighbours(source);
+
+Iterator itr=neighbours.iterator();
+
+
+			while(itr.hasNext()){
+				Edge e=(Edge)itr.next();
+				if(!visited[e.destination])
+				dfs(e.destination, input, visited);
+				//System.out.print(e.destination+"("+e.weight+")"+",");
+			}	
+	}
+}
+
 
 class GraphDriver{
 public static void main(String args[]){
 
 	InputStreamReader isr=new InputStreamReader(System.in);
 	BufferedReader br=new BufferedReader(isr);
-	Graph g=new Graph(4);
-	g.addEdge(0,1, 10);
-	g.addEdge(2,3, 30);
-	g.addEdge(2,1, 25);
-	System.out.println(g.getWeight(0,1));
-	g.display();
+	Graph g=new Graph(5);
+	g.addEdge(3,1, 10);
+	g.addEdge(0,3, 30);
+	g.addEdge(0,2, 25);
+	g.addEdge(3,4, 35);
+	g.addEdge(2,4, 45);
+	
+	//System.out.println(g.getWeight(0,1));
+	boolean visited[]=new boolean[g.size()];
+	for(int i=0;i<visited.length;i++){
+		visited[i]=false;	
+	}
+	GraphAlgorithms.dfs(0, g, visited);
+	//g.display();
 		
 		
 	}
